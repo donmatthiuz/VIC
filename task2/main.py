11857d7c00b12444336bb4d4ef50b194f570f6e9
CORDERO AQUINO <cor22982@uvg.edu.gt>
2026-02-09 20:46:19 -0600

    feat: Uso de sift

--- task2/main.py
+++ task2/main.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,7 +9,7 @@ def show_img(img, title="Imagen", cmap=None):
     # TODO: Matplotlib espera RGB, OpenCV carga BGR.
     # Verifica si la imagen tiene 3 canales y conviértela para visualización correcta.
     if len(img.shape) == 3 and cmap is None:
-        img_show = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_show = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     else:
         img_show = img
     
@@ -17,9 +17,71 @@ def show_img(img, title="Imagen", cmap=None):
     plt.axis('off')
     plt.show()
     
-    
-def main():
+
+def ORB_pipeline(frontal, side):
     pass
+
+def SIFT_pipeline(frontal, side):
+    sift = cv.SIFT_create()
+
+    kpnt1, des1 = sift.detectAndCompute(frontal, None)
+    kpnt2, des2 = sift.detectAndCompute(side, None)
+
+    bf = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
+    matches = bf.knnMatch(des1, des2, k=2)
+    good = []
+   
+    
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append([m])
+
+    matched_image = cv.drawMatchesKnn(
+            frontal,
+            kpnt1,
+            side,
+            kpnt2,
+            good,
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
+        )
+    return matched_image
+
+
+def main():
+    frontal = cv.imread('./data/close1.jpeg', cv.IMREAD_GRAYSCALE)
+    side = cv.imread('./data/far1.jpeg', cv.IMREAD_GRAYSCALE)
+
+    matched_sift = SIFT_pipeline(frontal, side)
+
+    original_pair = np.hstack((frontal, side))
+
+    plt.figure(figsize=(18, 6))
+
+    # Original (frontal + side)
+    plt.subplot(1, 3, 1)
+    plt.imshow(original_pair, cmap='gray')
+    plt.title('Imagen Original (Frontal + Side)')
+    plt.axis('off')
+
+    # SIFT
+    plt.subplot(1, 3, 2)
+    plt.imshow(matched_sift)
+    plt.title('SIFT – Buenos Matches')
+    plt.axis('off')
+
+    # ORB (placeholder)
+    plt.subplot(1, 3, 3)
+    plt.imshow(matched_sift)
+    plt.title('ORB – (placeholder)')
+    plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+    
 
 if __name__ == "__main__":
     main()
